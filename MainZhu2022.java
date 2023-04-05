274a022a55ede3cf3e058cd14d1fddec648b2b7d
dvanderrijst <donna@semi.technology>
2023-04-05 17:08:35 +0200

    first commit

---
+++ MainZhu2022.java
@@ -0,0 +1,5 @@
+public class MainZhu2022 {
+    public static void main(String[] args){
+        System.out.println("Hello donnie");
+    }
+}
---
+++ ZhuCplexModel.java
@@ -0,0 +1,12 @@
+import ilog.concert.IloException;
+import ilog.concert.IloNumExpr;
+import ilog.concert.IloNumVar;
+import ilog.cplex.IloCplex;
+
+public class ZhuCplexModel {
+    private IloCplex cplex;
+    private int T; 
+
+
+
+}
