9f29c24114cad1c5c801755aa7d56bff2047c5be
dvanderrijst <donna@semi.technology>
2023-04-12 17:44:23 +0200

    created combinations for T and n

--- Omega.java
+++ Omega.java
@@ -1,9 +1,14 @@
 public class Omega {
     private int T;
     private int n;
+    private int alpha;
+    private int beta;
+    private static double[] probs;
     public Omega(ZhuInstance instance) {
         this.T = instance.T;
         this.n = instance.n;
+        this.alpha = instance.alpha;
+        this.beta = instance.beta;
     }
 
     public int[][][] getTwir(){
@@ -24,175 +29,71 @@ public class Omega {
 
 
     public void createScenarios(){
-        this.T = 3;
-        this.n = 5;
-
-        int size = (int) Math.pow(T, n);
+        int size = (int) Math.pow(T+1, n);
         int[][] s = new int[size][n];
-
-        int V = T;
+        int V = T+1;
 
         for(int i = 0; i<n ; i++) {
-            //first
-            int R = (int) Math.pow(T, i);
-            int S = (int) Math.pow(T, n - i);
-            int D = (int) Math.pow(T, n - i - 1);
+            int R = (int) Math.pow(T+1, i);
+            int S = (int) Math.pow(T+1, n - i);
+            int D = (int) Math.pow(T+1, n - i - 1);
 
             for (int r = 0; r < R; r++) {
                 for (int v = 0; v < V; v++) {
-
                     int d = 0;
                     while (d < D) {
                         int i1 = r * S + v * D + d;
-                        if(i1 ==64){
-                            System.out.println("uo");
-                        }
-                        s[i1][i] = v;
+                        s[i1][i] = v+1;
                         d++;
                     }
                 }
             }
         }
-//
-//            //second
-//            R = (int) Math.pow(T, i);
-//            S = (int) Math.pow(T, T-i);
-//            D = (int) Math.pow(T, T-i-1);
-//
-//            for (int r = 0; r < R; r++) {
-//                for (int v = 0; v < V; v++) {
-//                    int d = 0;
-//                    while (d < D) {
-//                        s[r * S + v * D + d][1] = v;
-//                        d++;
-//                    }
-//                }
-//            }
-//
-//            //third
-//            R = (int) Math.pow(T, i);
-//            S = (int) Math.pow(T, T-i);
-//            D = (int) Math.pow(T, T-i-1);
-//
-//            for (int r = 0; r < R; r++) {
-//                for (int v = 0; v < V; v++) {
-//                    int d = 0;
-//                    while (d < D) {
-//                        s[r * S + v * D + d][2] = v;
-//                        d++;
-//                    }
-//                }
+//        System.out.print("printing the results:");
+//        for (int i = 0; i < size; i++) {
+//            System.out.println();
+//            for (int j = 0; j < n; j++) {
+//                System.out.print(s[i][j]+"\t");
 //            }
 //        }
 
-//        //first
-//        R = (int) Math.pow(T,0);
-//        S = (int) Math.pow(T,3);
-//        D = (int) Math.pow(T,2);
-//
-//        //second
-//        R = (int) Math.pow(T,1);
-//        S = (int) Math.pow(T,2);
-//        D = (int) Math.pow(T,1);
-//
-//        //third
-//        R = (int) Math.pow(T,2);
-//        S = (int) Math.pow(T,1);
-//        D = (int) Math.pow(T,0);
-//
-//
-//        R = [0 , 1, 2];
-//        S = [3, 2, 1];
-//        D = [2, 1, 0];
-//
+        System.out.println("Now we are linking the probabilities to it....");
+        double[] p_w = new double[size];
 
-
-
-
-
-
-
-//
-//        int v = 0;
-//        int d = 0;
-//        int durance = 1;
-//        while(v < T) {
-//            int c = 0;
-//            while (c < (int) Math.pow(T,2)) {
-//                d = 0;
-//                while(d<durance) {
-//                    s[v + (int) Math.pow(T, 1) * c + d][2] = v;
-//                    d++;
-//                }
-//                c = c + 1;
-//            }
-//            v++;
-//        }
-//
-//        v = 0;
-//        d = 0;
-//        durance = 3;
-//        while(v < T) {
-//            int c = 0;
-//            while (c < (int) Math.pow(T,1)) {
-//                d = 0;
-//                while(d<durance) {
-//                    s[(int) Math.pow(T, 2) * c + d][1] = v;
-//                    d++;
-//                }
-//                c = c + 1;
-//            }
-//            v++;
-//        }
-//
-//        v = 0;
-//        d = 0;
-//        durance = 27;
-//        while(v < T) {
-//            int c = 0;
-//            while (c < (int) Math.pow(T,0)) {
-//                d = 0;
-//                while(d<durance) {
-//                    s[(int) Math.pow(T, 2) * c + d][0] = v;
-//                    d++;
-//                }
-//                c = c + 1;
-//            }
-//            v++;
-//        }
-
-
-
-//        for(int i = 0; i < size - 1 ; i = i + (int) Math.pow(T,2)) {
-//            int add2 = 0;
-//            while (add2< (int) Math.pow(T,1)) {
-//                int add1 = 0;
-//                while (add1 < (int) Math.pow(T,1)) {
-//                    s[i + add1 + (int) Math.pow(T,1) * add2][1] = add2;
-//                    add1++;
-//                }
-//                add2++;
-//            }
-//        }
-
-//        for(int i = 0; i < size - 1; i = i + (int) Math.pow(T,3)){
-//            int add2 = 0;
-//            while(add2< (int) Math.pow(T,1)) {
-//                int add1 = 0;
-//                while (add1 < (int) Math.pow(T,2)) {
-//                    s[i + add1  + (int) Math.pow(T,2) * add2][0] = add2;
-//                    add1 = add1 + 1;
-//                }
-//                add2++;
-//            }
-//        }
-
-        System.out.println("this is the result");
+        double sum = 0.0;
         for (int i = 0; i < size; i++) {
-            System.out.println();
+            double probability = 1.0;
             for (int j = 0; j < n; j++) {
-                System.out.print(s[i][j]+"\t");
+                probability = probability * P_X(s[i][j]);
             }
+            p_w[i] = probability;
+            sum = sum + probability;
         }
+        System.out.println("The sum is "+sum+" if this is not equal to one, something goes wrong here.");
+    }
+
+    /**
+     * Returning the descretised Weibull dist:
+     * Here, de CDF is F(x)=1-exp((x/alpha)^beta). Then P(X=x)=F(x)-F(x-1)
+     * @param x
+     * @return P(X=x)
+     */
+    private double P_X(int x){
+        if(probs == null){
+            probs = new double[T+2];
+            double sum = 0.0;
+
+            for (int i = 1; i <= T ; i++) {
+                double hi = Math.pow(i / (double) alpha, beta);
+                double F_x = 1.0 - Math.exp(-1*hi);
+                double hii = Math.pow((i - 1) / (double) alpha,beta);
+                double F_x_1 = 1.0 - Math.exp(-1*hii);
+                probs[i] = F_x - F_x_1;
+                sum = sum + probs[i];
+            }
+            System.out.println(sum);
+            probs[T+1] = 1.0 - sum;
+        }
+        return probs[x];
     }
 }
--- ZhuInstance.java
+++ ZhuInstance.java
@@ -5,6 +5,8 @@ public class ZhuInstance {
     public final int[] cPR_i = new int[]{ 0, 1,2,3,4} ;
     public final int[] cCR_i = new int[]{ 0, 1,2,3,4} ;
     public final int d       = 3 ;
+    public final int alpha = 1;
+    public final int beta = 2;
 
     public ZhuInstance() {
     }
