2eccd08baafe4bfddf717ee55ff850662ed802a9
dvanderrijst <donna@semi.technology>
2023-04-13 17:49:47 +0200

    created combinations for T and n

--- ZhuInstance.java
+++ ZhuInstance.java
@@ -1,6 +1,6 @@
 public class ZhuInstance {
-    public final int T       = 5 ;
-    public final int n       = 2 ;
+    public final int T       = 3 ;
+    public final int n       = 1 ;
     public final int q       = 4 ;
     public final int[] cPR_i = new int[]{ 0, 1,2,3,4} ;
     public final int[] cCR_i = new int[]{ 0, 1,2,3,4} ;
