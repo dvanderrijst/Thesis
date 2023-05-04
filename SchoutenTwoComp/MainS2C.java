9b5e1c59bb06fb0fc390a557a7fb307a0ec9ab36
dvanderrijst <donna@semi.technology>
2023-05-04 17:00:36 +0200

    first start instance Schouten

--- SchoutenTwoComp/MainS2C.java
+++ SchoutenTwoComp/MainS2C.java
@@ -1,6 +1,6 @@
-package Schouten;
+package SchoutenTwoComp;
 
-public class MainSchouten {
+public class MainS2C {
     public static void main(String[] args){
         System.out.println("Hello world");
     }
